dc2a96994173cdb7d72b99b5413776c91336adde
Boris Klco <klco@pm.me>
2023-06-25 14:55:58 +0000

    still no idea...

--- src/Form.jsx
+++ src/Form.jsx
@@ -1,20 +1,18 @@
 import { useState } from "react";
-import Lines from "./Lines";
+import Line from "./Line";
 
 const Form = () => {
   const [lines, setLines] = useState([]);
 
   const addLine = () => {
-    setLines(lines => [...lines, <Lines />]);
+    setLines(lines => [...lines, <Line />]);
   };
 
   return (
     <div>
       <form>
-        <Lines />
-        {lines.map(line => {
-          {line}
-        })}
+        <Line />
+        {lines.map(line => (line))}
       </form>
       <button onClick={addLine}>
         <span className="material-symbols-outlined">list_alt_add</span>
--- src/Line.jsx
+++ src/Line.jsx
@@ -1,16 +1,16 @@
-const Line = (line) => {
+const Line = () => {
   return (
-    <div key={line} className="formLine">
+    <div  className="formLine">
       <div className="lineName">
         <input type="text" placeholder="Label Name" />
       </div>
       <div className="lineContent">
         <button>
           Text:
-          <input id={line} type="text" placeholder="input Text" disabled />{" "}
+          <input  type="text" placeholder="input Text" disabled />{" "}
         </button>
         <button>
-          Radio: <input id={line} type="radio" value="radio" disabled />{" "}
+          Radio: <input type="radio" value="radio" disabled />{" "}
         </button>
       </div>
       <span className="material-symbols-outlined">close</span>
